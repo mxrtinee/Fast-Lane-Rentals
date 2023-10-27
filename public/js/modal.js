901e021b925c78c09b05c92347c047258a95eeb3
Alexis Lopez <lopez111alex@gmail.com>
2023-10-27 12:33:58 -0700

    Added a modal function
    when button is clicked a modal pops up and prompts users to enter log in information

---
+++ public/js/modal.js
@@ -0,0 +1,18 @@
+// modal
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("logInBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
