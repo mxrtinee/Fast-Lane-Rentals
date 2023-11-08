8566b5250d28ce1b3416aecf0c95d49867f5b511
Martin <mxrtinee@user.noreply.github.com>
2023-11-07 18:17:11 -0800

    Added bookingForm to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,8 +6,6 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const userDataFilePath = path.join(__dirname, "userData.json");
 
-const { Review } = require("../models");
-
 // Route to render the homepage
 router.get("/homepage", async (req, res) => {
   try {
@@ -32,9 +30,9 @@ router.get("/about", (req, res) => {
 });
 
 // Define a route to render the booking form
-router.get("/bookings/new", (req, res) => {
+router.get('/bookings/new', (req, res) => {
   // Render the booking form and pass the car data to the template
-  res.render("bookingForm", { cars: carData });
+  res.render('bookingForm', { cars: carData });
 });
 
 // Route to render the login page
